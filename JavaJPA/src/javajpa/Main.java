97ec8d3c5577325b2a0cae7ba03360cf73c4aaee
Alex Melnyk <melnyk@codefire.com.ua>
2016-06-09 19:39:18 +0300

    Add CRUD functionality.

--- JavaJPA/src/javajpa/Main.java
+++ JavaJPA/src/javajpa/Main.java
@@ -1,14 +1,13 @@
 /*
  * CodeFireUA public license.
  */
-
 package javajpa;
 
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -20,21 +19,56 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("JavaJPAPU");
-        
+
         EntityManager manager = factory.createEntityManager();
-        
+
         // JPQL
-        Query query = manager.createQuery("SELECT p FROM Post p");
-        List resultList = query.getResultList();
-        
-        for (Object post : resultList) {
+        TypedQuery<Post> query = manager.createQuery("SELECT p FROM Post p", Post.class);
+        List<Post> resultList = query.getResultList();
+
+        for (Post post : resultList) {
             System.out.println(post);
         }
-        
+
+        //<editor-fold defaultstate="collapsed" desc="INSERT WITH JPA">
+//        Post post= new Post();
+//        post.setAuthor("Unknown");
+//        post.setTimestamp(new Date());
+//        post.setTitle("Simple post.");
+//        post.setContent("Text for testing...");
+//
+//        System.out.println(post.getId());
+//        manager.getTransaction().begin();
+//        manager.persist(post); // INSERT
+//        manager.getTransaction().commit();
+//        System.out.println(post.getId());
+//</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="UPDATE WITH JPA">
+//        int idx = (int)(Math.random() * resultList.size());
+//        Post randomPost = resultList.get(idx);
+//
+//        randomPost.setAuthor("Random Author");
+//
+//        manager.getTransaction().begin();
+//        manager.merge(randomPost);
+//        manager.getTransaction().commit();
+//</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="DELETE WITH JPA">
+
+//        int idx = (int)(Math.random() * resultList.size());
+//        Post randomPost = resultList.get(idx);
+//
+//        manager.getTransaction().begin();
+//        manager.remove(randomPost);
+//        manager.getTransaction().commit();
+//</editor-fold>
+
         factory.close();
-        
+
     }
 
 }
